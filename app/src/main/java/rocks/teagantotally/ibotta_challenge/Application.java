8492f76a116285dcb5ac51a9d09dcfbd7ed98865
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-30 17:26:48 -0600

    Application object

---
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/Application.java
@@ -0,0 +1,10 @@
+package rocks.teagantotally.ibotta_challenge;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by tglenn on 8/30/17.
+ */
+
+public class Application extends MultiDexApplication {
+}
