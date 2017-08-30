a52ce0addbeb7635732be6b01368f1cf889702d9
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-30 17:28:30 -0600

    UI classes

---
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/ui/ScreenView.java
@@ -0,0 +1,17 @@
+package rocks.teagantotally.ibotta_challenge.ui;
+
+/**
+ * Created by tglenn on 8/30/17.
+ */
+
+public interface ScreenView {
+    /**
+     * @return The name of the screen
+     */
+    String getName();
+
+    /**
+     * @return The route for this screen
+     */
+    String getRoute();
+}
