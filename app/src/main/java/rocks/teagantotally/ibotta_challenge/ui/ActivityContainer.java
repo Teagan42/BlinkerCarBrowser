6396ea96b78fc2b477efbb454081ee112a7899d0
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-30 17:27:54 -0600

    Activity contianer

---
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/ui/ActivityContainer.java
@@ -0,0 +1,18 @@
+package rocks.teagantotally.ibotta_challenge.ui;
+
+/**
+ * Created by tglenn on 8/30/17.
+ */
+
+public class ActivityContainer
+          extends BaseActivity {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getRoute() {
+        return null;
+    }
+}
