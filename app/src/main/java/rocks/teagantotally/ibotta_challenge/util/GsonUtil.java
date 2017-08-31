235890e192f9fbe87ab9290e9a5afb2691fc9df0
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-30 20:01:50 -0600

    Gson util

---
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/util/GsonUtil.java
@@ -0,0 +1,58 @@
+package rocks.teagantotally.ibotta_challenge.util;
+
+import android.annotation.SuppressLint;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+
+import rocks.teagantotally.ibotta_challenge.R;
+import rocks.teagantotally.ibotta_challenge.di.Injector;
+
+/**
+ * Created by tglenn on 8/30/17.
+ */
+
+public class GsonUtil {
+    public static final String FORMAT_DATE =
+              Injector.get()
+                      .applicationContext()
+                      .getString(R.string.date_time_format);
+    @SuppressLint("SimpleDateFormat")
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat(FORMAT_DATE);
+
+    private static final Gson serializer = new GsonBuilder()
+              .setDateFormat(FORMAT_DATE)
+              .create();
+
+    public static SimpleDateFormat getDateFormatter() {
+        return dateFormatter;
+    }
+
+    public static String serialize(Object source) {
+        return serializer.toJson(source);
+    }
+
+    public static Object deserialize(String json) {
+        return deserialize(json,
+                           Object.class);
+    }
+
+    public static <T> T deserialize(String json,
+                                    Class<T> classOfT) {
+        return serializer.fromJson(json,
+                                   classOfT);
+    }
+
+    public static <T> T deserialize(String json,
+                                    Type type) {
+        return serializer.fromJson(json,
+                                   type);
+    }
+
+    public static Gson getSerializer() {
+        return serializer;
+    }
+}
