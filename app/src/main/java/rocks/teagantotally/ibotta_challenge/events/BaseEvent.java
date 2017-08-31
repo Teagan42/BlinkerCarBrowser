a24839fc932d2133681fa6a38ba66f61a14c79c6
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-30 20:58:24 -0600

    Posting events to bus

--- app/src/main/java/rocks/teagantotally/ibotta_challenge/events/BaseEvent.java
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/events/BaseEvent.java
@@ -1,8 +1,18 @@
 package rocks.teagantotally.ibotta_challenge.events;
 
+import rocks.teagantotally.ibotta_challenge.di.Injector;
+
 /**
  * Created by tglenn on 8/30/17.
  */
 
 public class BaseEvent {
+    /**
+     * Post this event to the event bus
+     */
+    public void post() {
+        Injector.get()
+                .eventBus()
+                .post(this);
+    }
 }
