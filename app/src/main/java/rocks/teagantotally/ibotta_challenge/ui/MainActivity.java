ec7d17ff66bdeae4ae16b099c0de3b143b9e4c31
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-08-31 10:57:42 -0600

    Use data binding for main screen

--- app/src/main/java/rocks/teagantotally/ibotta_challenge/ui/MainActivity.java
+++ app/src/main/java/rocks/teagantotally/ibotta_challenge/ui/MainActivity.java
@@ -1,24 +1,30 @@
 package rocks.teagantotally.ibotta_challenge.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
 
 import rocks.teagantotally.ibotta_challenge.R;
+import rocks.teagantotally.ibotta_challenge.databinding.ActivityMainBinding;
 import rocks.teagantotally.ibotta_challenge.events.NavigationEvent;
 import rocks.teagantotally.ibotta_challenge.ui.fragments.RetailerListFragment;
 
 public class MainActivity
           extends BaseActivity {
 
+    private View.OnClickListener openClickListener =
+              new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                      new NavigationEvent(RetailerListFragment.ROUTE).post();
+                  }
+              };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new NavigationEvent(RetailerListFragment.ROUTE).post();
-            }
-        });
+        ActivityMainBinding binding =
+                  DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setOpenClickListener(openClickListener);
     }
 }
